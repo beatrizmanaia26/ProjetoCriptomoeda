37e6e53641972944f194e586f87198c42c506c5d
LuaBortko <133054603+LuaBortko@users.noreply.github.com>
2024-05-09 19:00:18 -0300

    Gui compra e venda de criptomoeda

--- src/view/SacarReais.java
+++ src/view/SacarReais.java
@@ -8,12 +8,12 @@ package view;
  *
  * @author luana
  */
-public class DepositoReais extends javax.swing.JFrame {
+public class SacarReais extends javax.swing.JFrame {
 
     /**
-     * Creates new form DepositoReais
+     * Creates new form SacarReais
      */
-    public DepositoReais() {
+    public SacarReais() {
         initComponents();
     }
 
@@ -26,26 +26,26 @@ public class DepositoReais extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        lblDeposito = new javax.swing.JLabel();
+        lblSaque = new javax.swing.JLabel();
         lblReais = new javax.swing.JLabel();
-        txtReaisDeposito = new javax.swing.JTextField();
-        btDepositar = new javax.swing.JButton();
+        txtReaisSaque = new javax.swing.JTextField();
+        btSacar = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenuMenu = new javax.swing.JMenu();
         jMenuItemMenu = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        lblDeposito.setFont(new java.awt.Font("Book Antiqua", 3, 24)); // NOI18N
-        lblDeposito.setText("Depositar reais na carteira de investimento");
+        lblSaque.setFont(new java.awt.Font("Book Antiqua", 3, 24)); // NOI18N
+        lblSaque.setText("Sacar reais da carteira de investimento");
 
         lblReais.setFont(new java.awt.Font("Book Antiqua", 1, 24)); // NOI18N
         lblReais.setText("R$");
 
-        txtReaisDeposito.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        txtReaisSaque.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
 
-        btDepositar.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
-        btDepositar.setText("Depositar");
+        btSacar.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        btSacar.setText("Sacar");
 
         jMenuMenu.setText("Menu");
 
@@ -61,34 +61,34 @@ public class DepositoReais extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(lblDeposito)
-                .addContainerGap())
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(lblReais)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(txtReaisDeposito, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(104, 104, 104)
-                .addComponent(btDepositar, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(lblReais)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txtReaisSaque, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(104, 104, 104)
+                        .addComponent(btSacar, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(28, 28, 28)
+                        .addComponent(lblSaque)))
+                .addContainerGap(43, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(lblDeposito, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(lblSaque, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lblReais)
-                    .addComponent(txtReaisDeposito, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btDepositar, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(25, Short.MAX_VALUE))
+                    .addComponent(txtReaisSaque, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btSacar, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(44, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-  
 //    public static void main(String args[]) {
 //        /* Set the Nimbus look and feel */
 //        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -103,31 +103,31 @@ public class DepositoReais extends javax.swing.JFrame {
 //                }
 //            }
 //        } catch (ClassNotFoundException ex) {
-//            java.util.logging.Logger.getLogger(DepositoReais.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//            java.util.logging.Logger.getLogger(SacarReais.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 //        } catch (InstantiationException ex) {
-//            java.util.logging.Logger.getLogger(DepositoReais.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//            java.util.logging.Logger.getLogger(SacarReais.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 //        } catch (IllegalAccessException ex) {
-//            java.util.logging.Logger.getLogger(DepositoReais.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//            java.util.logging.Logger.getLogger(SacarReais.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 //        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-//            java.util.logging.Logger.getLogger(DepositoReais.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//            java.util.logging.Logger.getLogger(SacarReais.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 //        }
 //        //</editor-fold>
 //
 //        /* Create and display the form */
 //        java.awt.EventQueue.invokeLater(new Runnable() {
 //            public void run() {
-//                new DepositoReais().setVisible(true);
+//                new SacarReais().setVisible(true);
 //            }
 //        });
 //    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btDepositar;
+    private javax.swing.JButton btSacar;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItemMenu;
     private javax.swing.JMenu jMenuMenu;
-    private javax.swing.JLabel lblDeposito;
     private javax.swing.JLabel lblReais;
-    private javax.swing.JTextField txtReaisDeposito;
+    private javax.swing.JLabel lblSaque;
+    private javax.swing.JTextField txtReaisSaque;
     // End of variables declaration//GEN-END:variables
 }
