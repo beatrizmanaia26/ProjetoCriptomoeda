cbf8d2bdbaf8f71449d0a26285124e6b72f476e9
LuaBortko <133054603+LuaBortko@users.noreply.github.com>
2024-05-13 20:05:30 -0300

    adição conexao dnv

--- src/DAO/Conexao.java
+++ src/DAO/Conexao.java
@@ -4,10 +4,19 @@
  */
 package DAO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author luana
  */
 public class Conexao {
-    
+    public Connection getConnection() throws SQLException{
+        Connection conexao = DriverManager.getConnection(
+        "jdbc:postgresql://localhost:5432/Criptomoedas","postgres","fei");
+                                                                //Mudar senha
+        return conexao;
+    }
 }
